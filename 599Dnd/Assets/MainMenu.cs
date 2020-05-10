00c8b5defd50a7d9ecc783ae924ef35d95f5d5e1
Fabio <verto95@gmil.com>
2020-05-09 19:38:50 -0600

    Moving Project

---
+++ 599Dnd/Assets/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SceneSelection()
+    {
+        SceneManager.LoadScene("SelectionMenu");
+    }
+
+    public void ForestSelection()
+    {
+        SceneManager.LoadScene("ForestScene");
+    }
+
+    public void SnowSelection()
+    {
+        SceneManager.LoadScene("SnowScene");
+    }
+
+    public void SandSelection()
+    {
+        SceneManager.LoadScene("SandScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();   
+    }
+}
---
+++ 599Dnd/Assets/effects_controller_script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effects_controller_script : MonoBehaviour
+{
+    [SerializeField] ParticleSystem[] StatusEffects;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (ParticleSystem status in StatusEffects)
+            status.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            if (StatusEffects[0]) {
+                if (StatusEffects[0].isStopped)
+                    StatusEffects[0].Play();
+                else
+                    StatusEffects[0].Stop();
+                }
+        }
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            if (StatusEffects[1])
+            {
+                if (StatusEffects[1].isStopped)
+                    StatusEffects[1].Play();
+                else
+                    StatusEffects[1].Stop();
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            if (StatusEffects[2])
+            {
+                if (StatusEffects[2].isStopped)
+                    StatusEffects[2].Play();
+                else
+                    StatusEffects[2].Stop();
+            }
+        }
+    }
+}
